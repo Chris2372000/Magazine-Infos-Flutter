e4c637ae46465966718cb0ff14e863d1e98abecd
Chris2372000 <sianichristian237@gmail.com>
2026-06-22 11:22:48 +0200

    Version 4.1 - premiere interface Magazine Infos

--- lib/main.dart
+++ lib/main.dart
@@ -9,16 +9,20 @@ class MonAppli extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Magazine',
       debugShowCheckedModeBanner: false,
-      home: PageAccueil(),
+      home: const PageAccueil(),
     );
   }
 }
 
 class PageAccueil extends StatelessWidget {
   const PageAccueil({super.key});
+
+  void afficherMessage() {
+    print('Tu as cliqué dessus');
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -39,163 +43,22 @@ class PageAccueil extends StatelessWidget {
         ],
       ),
 
-      body: const SingleChildScrollView(
-        child: Column(
-          children: [
-            Image(
-              image: AssetImage(
-                'assets/magazine.jpg',
-              ),
-            ),
-
-            PartieTitre(),
-            PartieTexte(),
-            PartieIcone(),
-            PartieRubrique(),
-          ],
+      body: Center(
+        child: Image.asset(
+          'assets/magazine.jpg',
         ),
       ),
 
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.pink,
         onPressed: () {
-          debugPrint('Tu as cliqué dessus');
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(
+              content: Text('Tu as cliqué dessus'),
+            ),
+          );
         },
         child: const Text('Click'),
-      ),
-    );
-  }
-}
-
-class PartieTitre extends StatelessWidget {
-  const PartieTitre({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      padding: const EdgeInsets.symmetric(
-        horizontal: 20,
-        vertical: 20,
-      ),
-      child: const Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            'Magazine Infos',
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-
-          SizedBox(height: 8),
-
-          Text(
-            'Le magazine numérique de référence',
-            style: TextStyle(
-              fontSize: 18,
-              color: Colors.grey,
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class PartieTexte extends StatelessWidget {
-  const PartieTexte({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(20),
-      child: const Text(
-        'Magazine Infos est un magazine numérique moderne qui '
-        'propose à ses lecteurs des informations variées sur '
-        'la mode, la culture, la presse et les tendances actuelles.',
-        textAlign: TextAlign.justify,
-        style: TextStyle(fontSize: 16),
-      ),
-    );
-  }
-}
-
-class PartieIcone extends StatelessWidget {
-  const PartieIcone({super.key});
-
-  Widget creerElement(IconData icone, String texte) {
-    return Column(
-      children: [
-        Icon(
-          icone,
-          color: Colors.pink,
-          size: 30,
-        ),
-
-        const SizedBox(height: 5),
-
-        Text(
-          texte,
-          style: const TextStyle(
-            color: Colors.pink,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      ],
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(bottom: 10),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          creerElement(Icons.phone, 'TEL'),
-          creerElement(Icons.mail, 'MAIL'),
-          creerElement(Icons.share, 'PARTAGE'),
-        ],
-      ),
-    );
-  }
-}
-
-class PartieRubrique extends StatelessWidget {
-  const PartieRubrique({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(
-        horizontal: 20,
-        vertical: 20,
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(15),
-            child: Image.asset(
-              'assets/presse.jpg',
-              width: 150,
-              height: 100,
-              fit: BoxFit.cover,
-            ),
-          ),
-
-          ClipRRect(
-            borderRadius: BorderRadius.circular(15),
-            child: Image.asset(
-              'assets/mode.jpg',
-              width: 150,
-              height: 100,
-              fit: BoxFit.cover,
-            ),
-          ),
-        ],
       ),
     );
   }
